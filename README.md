dc52e0784e28fa3524c0fadedef116d13d083427
MaoXinTeng <44192906+mxtadmin@users.noreply.github.com>
2021-01-25 17:49:20 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,58 @@
+# Project description
+
+This is a tool for processing time.
+
+
+
+# Installation
+
+It is possible to install the tool with pip:
+
+```
+pip install xt-date-utils
+```
+
+
+
+# Feature
+
+Time processing module based on *`datetime`* and *`arrow`*
+
+
+
+# Configuration
+
+The script itself is currently configuration free.
+
+
+
+# Dependencies
+
+- arrow
+- datetime
+
+
+
+# Usage
+
+Sample usage:
+
+```python
+from xt-date-utils import DateUtils
+
+str_to_datetime = DateUtils.str_to_datetime('2021-01-01', '%Y-%m-%d')
+```
+
+
+
+# Changes
+
+**Version 0.0.1**
+
+- Initial version
+
+
+
+# License
+
+The script is released under the MIT License.The MIT License is registered with and approved by the Open Source Initiative.
---
+++ setup.py
@@ -0,0 +1,28 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Author : mao
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xt-date-utils",
+    version="0.0.1",
+    author="Maoxinteng",
+    author_email="1214403402@qq.com",
+    description="A Tool For Processing Time",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Natural Language :: Chinese(Simplified)",
+        "Development Status :: 5 - Production/Stable"
+    ],
+    python_requires=">=3.6"
+)
