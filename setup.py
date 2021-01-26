2d5a73e15fb5bd3e52dd768e2b4ef55297a7f979
兔子先生😉 <oncwnuDUXQn4lX_NOOE1j8nFqJVs@git.weixin.qq.com>
2021-01-26 10:13:56 +0800

    修改配置信息

--- setup.py
+++ setup.py
@@ -22,5 +22,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],
-    python_requires=">=3.6"
+    python_requires=">=3.6",
+    install_requires=["arrow"],
 )
