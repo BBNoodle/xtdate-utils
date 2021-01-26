80a95f507644be8272fd6e0aeb7f681043f496af
兔子先生😉 <oncwnuDUXQn4lX_NOOE1j8nFqJVs@git.weixin.qq.com>
2021-01-26 09:53:40 +0800

    修改配置信息

--- setup.py
+++ setup.py
@@ -18,9 +18,9 @@ setuptools.setup(
     url="https://github.com/mxtadmin/date-utils",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],
-    python_requires=">=3.7"
+    python_requires=">=3.6"
 )
