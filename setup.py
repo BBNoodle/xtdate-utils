a1a81088d485d552724492fe04525d5c1889b2ab
兔子先生😉 <oncwnuDUXQn4lX_NOOE1j8nFqJVs@git.weixin.qq.com>
2021-01-26 15:59:55 +0800

    修改配置信息

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="xt-TimeUtils",
-    version="0.0.1",
+    version="0.1.0",
     author="Maoxinteng",
     author_email="1214403402@qq.com",
     description="A Tool For Processing Iteration Time",
